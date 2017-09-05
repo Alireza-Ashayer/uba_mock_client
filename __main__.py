e088c4dabf12113ec6e255ef9ed8c40f84b13d43
Alireza Ashayer <progade@gmail.com>
2017-09-05 18:58:34 -0400

    implement the request sender logic using threads added url as parameter

--- __main__.py
+++ __main__.py
@@ -1,17 +1,59 @@
 import sys
 import os
 import random
+import time
 from optparse import OptionParser
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from threading import Thread
 
-parser = OptionParser()
 
-parser.add_option("-r", "--rate", action="store", dest="rate")
-(options, args) = parser.parse_args()
+def send_request(url):
+    post_fields = {"Category": "swipe"}
 
-rate = options.rate
+    request = Request(url, urlencode(post_fields).encode())
+    return urlopen(request)
 
-if rate is None:
-    raise ValueError("Please specify rate [-r]")
+
+def send_requests(url, rate):
+    for i in range(0, int(rate)):
+        Thread(target=send_request, args=[url]).start()
+
+    print("%s requests sent" % rate)
+
+
+def main():
+
+    # parse parameters
+    parser = OptionParser()
+    parser.add_option("-r", "--rate", action="store", dest="rate")
+    parser.add_option("-u", "--url", action="store", dest="url")
+    (options, args) = parser.parse_args()
+    rate = options.rate
+    url = options.url
+
+    # check parameters validity
+    if rate is None:
+        raise ValueError("Please specify request rate per second using -r or --rate")
+
+    try:
+        int(rate)
+    except ValueError:
+        raise ValueError("rate is not of integer type")
+
+    if url is None:
+        raise ValueError("Please specify a url using -u or --url")
+
+    # start a timer to send requests
+    while True:
+        Thread(target=send_requests, args=[url, rate]).start()
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
